caf598f6946cd5ff4dfb98f16a51544ce7eb2d44
BETA1234567 <139150546+BETA1234567@users.noreply.github.com>
2023-07-10 11:32:05 -0400

    Add files via upload

---
+++ BrandonTapia_PGY1121_001_D.py
@@ -0,0 +1,58 @@
+from os import system
+system("cls")
+
+input=print["asientos":]("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25")
+op=input["asientos":]
+
+while True:
+    print("""
+    ************BIENVENIDO A CREATIVO.CL**********
+    1.comprar entradas 
+    2.mostrar ubicaciones disponibles 
+    3.ver listado de asistentes
+    4.mostrar ganancias totales
+    5.salir del programa
+    """)
+
+    op=input("ingrese una opción: \n")
+    match op:
+        case "1":
+            pass
+        case "2":
+            pass
+        case "3":
+            pass
+        case "4":
+            pass
+        case "5":
+            break
+    input=print("selecione una opcion para continuar")
+
+
+while True:
+    print("""
+********¿QUE ENTRADA COMPRARA?********
+1.platinum ($120.000)  (asientos de 1 al 20)
+2.gold ($80.000)  (asientos del 21 al 50) 
+3.silver ($50.000)  (asientos del 51 al 100) 
+4.salir 
+        """)
+
+    op=input("ingrese una opción \n")
+    match op:
+        case "1":
+            pass
+        case "2":
+            pass
+        case "3":
+            pass
+        case "4":
+            break
+    input=print("seleccionar una opción por favor")
+
+
+op=input("ingrese una opción: ")
+match op:
+        case "1":
+            rut=input
+            
